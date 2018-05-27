dbbd3c171edebe76e02108e2c48ca516926c3f5e
Chaudois <damien.chaudois@gmail.com>
2018-05-27 21:05:47 +0200

    rajout du tab

--- projetDesignPatern4AL1G16/MainWindow.Designer.cs
+++ projetDesignPatern4AL1G16/MainWindow.Designer.cs
@@ -31,22 +31,59 @@ namespace WinForm
         {
             this.mainPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.InfoPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
             this.mainPanel.AutoScroll = true;
-            this.mainPanel.Location = new System.Drawing.Point(12, 12);
+            this.mainPanel.Location = new System.Drawing.Point(6, 6);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(634, 576);
+            this.mainPanel.Size = new System.Drawing.Size(614, 535);
             this.mainPanel.TabIndex = 0;
             // 
             // InfoPanel
             // 
-            this.InfoPanel.Location = new System.Drawing.Point(652, 12);
+            this.InfoPanel.Location = new System.Drawing.Point(626, 6);
             this.InfoPanel.Name = "InfoPanel";
-            this.InfoPanel.Size = new System.Drawing.Size(379, 576);
+            this.InfoPanel.Size = new System.Drawing.Size(379, 535);
             this.InfoPanel.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1019, 576);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.mainPanel);
+            this.tabPage1.Controls.Add(this.InfoPanel);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1011, 547);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Contact";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1011, 547);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Import/Export";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
@@ -54,11 +91,12 @@ namespace WinForm
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1043, 600);
-            this.Controls.Add(this.InfoPanel);
-            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.tabControl1);
             this.Name = "MainWindow";
             this.Text = "Form1";
             this.Shown += new System.EventHandler(this.MainWindow_Shown);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -67,6 +105,9 @@ namespace WinForm
 
         private FlowLayoutPanel mainPanel;
         private FlowLayoutPanel InfoPanel;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
 
