b6dd5386136cb46e3ef303030d882bfcfe75a9ef
Chaudois <damien.chaudois@gmail.com>
2018-07-01 22:07:14 +0200

    ajout des tests

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using BO;
+using DAL;
+using DTO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Linq;
 namespace Test
 {
     [TestClass]
@@ -33,5 +36,173 @@ namespace Test
                 Assert.AreEqual("nissan micra", result[2].fields[2].value);
             }
         }
+        [TestMethod]
+        public void should_export_csv()
+        {
+            List<ContactDTO> listToExport = new List<ContactDTO>
+            {
+                new ContactDTO
+                {
+                    id=0,
+                    firstName="firstnametest1",
+                    lastName="lastnametest1",
+                    fields=new List<FieldDTO>
+                    {
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest1",
+                            value="valuetest1"
+                        },
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest2",
+                            value="valuetest2"
+                        },
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest3",
+                            value="valuetest3"
+                        }
+
+                    }
+                },new ContactDTO
+                {
+                    id=1,
+                    firstName="firstnametest2",
+                    lastName="lastnametest2",
+                    fields=new List<FieldDTO>
+                    {
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest1",
+                            value="valuetest4"
+                        },
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest2",
+                            value="valuetest5"
+                        },
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest3",
+                            value="valuetest6"
+                        }
+
+                    }
+                }
+            };
+            new Export(new CSVWork(), listToExport).ExportContact();
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(@"C:\Users\d.chaudois\source\repos\projetDesignPatern4AL1G16\test\bin\Debug\exportCSV-" + DateTime.Now.ToString("dd-MM-yyyy") + ".txt"))
+            {
+                string dataToImport = sr.ReadToEnd();
+                Assert.IsTrue(dataToImport == "firstName,lastName,fieldtest1,fieldtest2,fieldtest3\nfirstnametest1,lastnametest1,valuetest1,valuetest2,valuetest3\nfirstnametest2,lastnametest2,valuetest4,valuetest5,valuetest6\n");
+            }
+
+
+        }
+        [TestMethod]
+        public void should_export_json()
+        {
+            List<ContactDTO> listToExport = new List<ContactDTO>
+            {
+                new ContactDTO
+                {
+                    id=0,
+                    firstName="firstnametest1",
+                    lastName="lastnametest1",
+                    fields=new List<FieldDTO>
+                    {
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest1",
+                            value="valuetest1"
+                        },
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest2",
+                            value="valuetest2"
+                        },
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest3",
+                            value="valuetest3"
+                        }
+
+                    }
+                },new ContactDTO
+                {
+                    id=1,
+                    firstName="firstnametest2",
+                    lastName="lastnametest2",
+                    fields=new List<FieldDTO>
+                    {
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest1",
+                            value="valuetest4"
+                        },
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest2",
+                            value="valuetest5"
+                        },
+                        new FieldDTO
+                        {
+                            idContact=0,
+                            name="fieldtest3",
+                            value="valuetest6"
+                        }
+
+                    }
+                }
+            };
+            new Export(new JSONWork(), listToExport).ExportContact();
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(@"C:\Users\d.chaudois\source\repos\projetDesignPatern4AL1G16\test\bin\Debug\exportJSON-" + DateTime.Now.ToString("dd-MM-yyyy") + ".txt"))
+            {
+                string dataToImport = sr.ReadToEnd();
+                Assert.IsTrue(dataToImport == "[{\"firstName\":\"firstnametest1\",\"lastName\":\"lastnametest1\",\"fields\":[{\"name\":\"fieldtest1\",\"value\":\"valuetest1\",\"idContact\":\"0\"},{\"name\":\"fieldtest2\",\"value\":\"valuetest2\",\"idContact\":\"0\"},{\"name\":\"fieldtest3\",\"value\":\"valuetest3\",\"idContact\":\"0\"}]},{\"firstName\":\"firstnametest2\",\"lastName\":\"lastnametest2\",\"fields\":[{\"name\":\"fieldtest1\",\"value\":\"valuetest4\",\"idContact\":\"0\"},{\"name\":\"fieldtest2\",\"value\":\"valuetest5\",\"idContact\":\"0\"},{\"name\":\"fieldtest3\",\"value\":\"valuetest6\",\"idContact\":\"0\"}]}]");
+            }
+
+
+        }
+        [TestMethod]
+        public void should_save_and_remove_new_contact()
+        {
+            new ContactSQL().Add(new ContactDTO
+            {
+                firstName = "testinsertfirstaname",
+                lastName = "testinsertlastname",
+                fields = new List<FieldDTO>
+                {
+                    new FieldDTO
+                    {
+                        name="testfield1",
+                        value="testvalue1"
+                    }
+                }
+            });
+            var lastContactAdded = new ContactSQL().GetAll().OrderBy(e=>e.id).Single();
+
+            Assert.IsTrue(lastContactAdded.firstName == "testinsertfirstaname");
+            Assert.IsTrue(lastContactAdded.lastName == "testinsertlastname");
+            Assert.IsTrue(lastContactAdded.fields[0].name == "testfield1");
+            Assert.IsTrue(lastContactAdded.fields[0].value == "testvalue1");
+            new ContactSQL().remove(lastContactAdded);
+            var allContact = new ContactSQL().GetAll();
+            Assert.IsTrue(allContact.Count()==0);
+
+        }
+
     }
 }
